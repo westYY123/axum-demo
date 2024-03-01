3bd26ac923f852613feb63fbc23e5034094e2e1b
whyy7777 <yinyhuang7@gmail.com>
2024-03-02 00:44:42 +0800

    add ascii art

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,23 @@ async fn main() -> Result<(), AppError> {
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
-    Ok(axum::serve(listener, app)
+    print_start_info();
+    axum::serve(listener, app)
         .await
-        .map_err(|_| panic!("start server error"))?)
+        .map_err(|_| panic!("start server error"))?;
+
+    Ok(())
+}
+
+fn print_start_info() {
+    eprintln!("start listening on port 3000");
+    let ascii = r"
+    _____                                 ________                         
+    /  _  \ ___  _____ __  _____           \______ \   ____   _____   ____  
+   /  /_\  \\  \/  /  |  \/     \   ______  |    |  \_/ __ \ /     \ /  _ \ 
+  /    |    \>    <|  |  /  Y Y  \ /_____/  |    `   \  ___/|  Y Y  (  <_> )
+  \____|__  /__/\_ \____/|__|_|  /         /_______  /\___  >__|_|  /\____/ 
+          \/      \/           \/                  \/     \/      \/        
+    ";
+    eprintln!("{ascii}");
 }
