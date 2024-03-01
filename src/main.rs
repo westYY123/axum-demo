9b1d8df0f15070e9e61184332868a31fb8373531
whyy7777 <yinyhuang7@gmail.com>
2024-03-02 00:39:23 +0800

    add return type to main function

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
 use std::fs;
 
-use axum_demo::{route::create_route, setting::AppConfig};
+use axum_demo::{error::AppError, route::create_route, setting::AppConfig};
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), AppError> {
     let file_contents =
         fs::read_to_string("/Users/yiny/rust_projects/axum-demo/src/config.example.yaml")
             .expect("LogRocket: Should have been able to read the file");
-    let config: AppConfig = serde_yaml::from_str(&file_contents).unwrap();
+    let config: AppConfig =
+        serde_yaml::from_str(&file_contents).map_err(|_| panic!("read config failed"))?;
 
     let app = create_route(config).await;
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
-    axum::serve(listener, app).await.unwrap();
+    Ok(axum::serve(listener, app)
+        .await
+        .map_err(|_| panic!("start server error"))?)
 }
